e645917d3069f6b668121e2f9e8b4039068d8368
Saurabh <saurabh.baid@cambiumnetworks.com>
2021-02-09 04:54:08 +0000

    Added GetFromMap function to get nested keys from the map[string]interface{}

--- utils/utils.go
+++ utils/utils.go
@@ -1,6 +1,9 @@
 package utils
 
-import "strings"
+import (
+	"strings"
+	"encoding/json"
+)
 
 // StringArrayContains find in string array
 //
@@ -20,4 +23,41 @@ func StringArrayContains(s []string, e string, casecare bool) bool {
 		}
 	}
 	return false
+}
+
+
+// GetFromMap look for the keys specified by []string. 
+// it will iterate through each keys recursively and return the final value as interface
+// if the value is not found it will return nil.
+func GetFromMap(m map[string]interface{}, k []string, deepParse bool) (interface{}, bool) {
+	if len(k) == 0 {
+		return nil, false
+	}
+	
+	key := k[0]
+	k = k[1:]
+	
+	ret, ok := m[key]
+	
+	if len(k) == 0 {
+		return ret, ok
+	}
+
+	ret1, isMap := ret.(map[string]interface{})
+	if !isMap {
+		if !deepParse {
+			return nil, false
+		}
+		str, isString := ret.(string)
+		if !isString {
+			return nil, false
+		} 
+		var ret1 map[string]interface{}
+		if err := json.Unmarshal([]byte(str), &ret1); err != nil {
+			return nil, false
+		}
+		return GetFromMap(ret1, k, deepParse)
+	}
+	return GetFromMap(ret1, k, deepParse)	
+
 }
--- utils/utils_test.go
+++ utils/utils_test.go
@@ -23,3 +23,46 @@ func Example_StringArrayContains_IgnoreCase() {
 	// true
 	// false
 }
+
+
+func Example_GetFromMap() {
+	in := map[string]interface{}{
+	  "k1": "v1",
+	  "k2": map[string]interface{}{
+		  "ik2": "iv2",
+	  },
+	  "k3": `{"ik3":"iv3"}`,
+	  "k4": map[string]interface{}{
+		  "ik4":  map[string]interface{}{
+			  "iik4":  map[string]interface{}{
+				  "iiik4": "v4",
+			  },
+		  },
+	  },
+	  "k5": `{
+		"ik5" : {
+			"iik5": {
+				"iiik5": "v5"
+			}
+		}}`,
+	}
+	fmt.Println(GetFromMap(in, []string{""}, false))
+	fmt.Println(GetFromMap(in, []string{"k0"}, false))
+	fmt.Println(GetFromMap(in, []string{"k1"}, false))
+	fmt.Println(GetFromMap(in, []string{"k2", "ik2"}, false))
+	fmt.Println(GetFromMap(in, []string{"k2", "ik3"}, false))
+	fmt.Println(GetFromMap(in, []string{"k3", "ik3"}, false))
+	fmt.Println(GetFromMap(in, []string{"k3", "ik3"}, true))
+	fmt.Println(GetFromMap(in, []string{"k4", "ik4", "iik4", "iiik4"}, false))
+	fmt.Println(GetFromMap(in, []string{"k5", "ik5", "iik5", "iiik5"}, true))
+	//output:
+	// <nil> false
+	// <nil> false
+	// v1 true
+	// iv2 true
+	// <nil> false
+	// <nil> false
+	// iv3 true
+	// v4 true
+	// v5 true
+}
